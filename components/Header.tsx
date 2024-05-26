5bc1dd73d10945d70053b8ca22ec17b1a649879c
level237 <Bramslevel129@gmail.com>
2024-05-26 19:27:39 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Button } from './ui/button'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <>
     <section className='max-h-36   mt-5 flex mx-8 items-center justify-between'>
-      <div className=''>
-        <h2 className='text-2xl text-white font-bold'>SkillCat</h2>
+      <div className='flex items-center gap-3'>
+        <Image className='rounded-full' width="30" height="30" src="/img3.jpg" alt={''}/>
+        <h2 className='text-2xl text-white font-bold'>Agence XYZ</h2>
       </div>
       <ul className='flex text-white gap-12 justify-between'>
         <li>
-          Pricing
+         Formations
         </li>
         <li>
-          Review
+         Solutions digitales
         </li>
         <li>
-          Showcase
+          Portfolio
         </li>
         <li>
-         Blog
+        Contactez nous
         </li>
       </ul>
       <div>
